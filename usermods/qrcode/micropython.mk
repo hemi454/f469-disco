4ea820bd610327d4d6079a40a4cd4d07df02db98
Mikhail Sidorenko <msidorenko@embedity.dev>
2021-10-21 17:08:51 +0300

    Add submodule Quirc rev2

--- usermods/qrcode/micropython.mk
+++ usermods/qrcode/micropython.mk
@@ -3,6 +3,11 @@ QR_MOD_DIR := $(USERMOD_DIR)
 # Add all C files to SRC_USERMOD.
 SRC_USERMOD += $(QR_MOD_DIR)/qrcodegen/qrcodegen.c
 SRC_USERMOD += $(QR_MOD_DIR)/qrcode.c
+SRC_USERMOD += $(QR_MOD_DIR)/qrdecoder/lib/decode.c
+SRC_USERMOD += $(QR_MOD_DIR)/qrdecoder/lib/identify.c
+SRC_USERMOD += $(QR_MOD_DIR)/qrdecoder/lib/quirc.c
+SRC_USERMOD += $(QR_MOD_DIR)/qrdecoder/lib/version_db.c
 
 # We can add our module folder to include paths if needed
 CFLAGS_USERMOD += -I$(QR_MOD_DIR)/qrcodegen
+CFLAGS_USERMOD += -I$(QR_MOD_DIR)/qrdecoder/lib
